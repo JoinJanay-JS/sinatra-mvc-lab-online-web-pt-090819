c59dfe5fc29abafce7fa691eb6ca05069cfeec42
Janay Bailey <tjanay87@gmail.com>
2019-12-31 21:40:11 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,9 +13,7 @@ class PigLatinizer
     end
   end
 
-  #if the word starts with a vowel add yay to the end
-  #if the word starts with consonant(s) add the consonants up to the next vowel and ay to the end
-  def to_pig_latin(phrase)
-    phrase.split.collect {|word| piglatinize(word)}.join(' ')
+  def to_pig_latin(sentence)
+      sentence.split.collect {|word| piglatinize(word)}.join(' ')
   end
 end
